2630109a502a33912333789a39c687de5173448a
Yassine JOUT <yassinejout@gmail.com>
2018-11-25 12:12:37 +0100

    LoginViewModel File

---
+++ app/src/main/java/ma/fstm/ilisi/pico/picomobile/ViewModel/LoginViewModel.java
@@ -0,0 +1,45 @@
+package ma.fstm.ilisi.pico.picomobile.ViewModel;
+
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import ma.fstm.ilisi.pico.picomobile.BR;
+import ma.fstm.ilisi.pico.picomobile.Model.User;
+
+public class LoginViewModel extends BaseObservable {
+    private User user;
+
+    private String successMessage = "Login was successful";
+    private String errorMessage = "Email or Password not valid";
+
+    @Bindable
+    public String toastMessage = null;
+
+    public String getToastMessage() {
+        return toastMessage;
+    }
+    private void setToastMessage(String toastMessage) {
+
+        this.toastMessage = toastMessage;
+        notifyPropertyChanged(BR.toastMessage);
+    }
+    public LoginViewModel() {
+        user = new User("", "");
+    }
+
+    public void afterEmailTextChanged(CharSequence s) {
+        user.setEmail(s.toString());
+    }
+
+    public void afterPasswordTextChanged(CharSequence s) {
+        user.setPassword(s.toString());
+    }
+
+    public void onLoginClicked() {
+        if (user.isInputDataValid())
+            setToastMessage(successMessage);
+        else
+            setToastMessage(errorMessage);
+    }
+}
